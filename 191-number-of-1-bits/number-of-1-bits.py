6091787ef25fa49460efe1060bf8d0e702f99183
J Pavan kumar <154326138+PavankumarJ02@users.noreply.github.com>
2026-09-01 21:24:24 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (56.60%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -1,11 +1,12 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         binary = bin(n)[2:]
-        countlist =[int(x) for x in binary] 
+        '''countlist =[int(x) for x in binary] '''
         totalcount=0
-        total=0
-        for i in range(len(countlist)):
-            if countlist[i] == 1:
+       
+
+        for i in binary:
+            if i == "1":
                 totalcount+=1
             
 
