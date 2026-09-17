ee699b28053926d194a98a29c6732191262af175
J Pavan kumar <154326138+PavankumarJ02@users.noreply.github.com>
2026-09-17 23:03:03 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (52.23%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,9 +1,6 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        '''num_a = int(a,2)
-        num_b = int(b,2)
-        total = num_a+num_b
-        return bin(total)[2:]'''
+        
         tep_a=0
         tep_b=0
 
